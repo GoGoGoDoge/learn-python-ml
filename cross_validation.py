40a4ffa4c582df30f0c9558c1c960973006820e6
Guangyu Feng <fenggy0907@gmail.com>
2017-09-01 01:12:02 -0400

    for trying to run the overall_test

--- cross_validation.py
+++ cross_validation.py
@@ -1,3 +1,71 @@
+#!/usr/bin/python
+import re
+from pyclustering.samples.definitions import SIMPLE_SAMPLES, FCPS_SAMPLES;
+
+from pyclustering.cluster import cluster_visualizer;
+# from pyclustering.cluster.xmeans import xmeans, splitting_type;
+from xmeans import xmeans, splitting_type
+from pyclustering.utils import read_sample, timedcall;
+p=re.compile(r'(\d)([xy])')
+q=re.compile(r'xy')
+
+p=re.compile(r'(\d)([xy])')
+q=re.compile(r'xy')
+
+def insert_ast(s):
+    # Inserts asterisks * for sympy: xy -> x*y, 3x -> 3*x
+    return re.sub(q, r'x*y', re.sub(p, r'\1*\2', s))
+
+import sympy
+x,y=sympy.symbols("x y")
+
+file_name = 'colon-cancer.kernel'
+d = 134; # Dimension of the gram matrix = Number of samples
+labels = {} # Key: Sample ID; Value: Class label
+gram_mat_func = {} # Key: Pair of sample IDs; Value: SymPy object of kernel value
+
+def set_elmnt(i, j, s):
+    # Sets a SymPy object representing a string s as an (i, j) element
+    gram_mat_func[(min([i,j]),max([i,j]))]=sympy.sympify(s)
+
+def get_elmnt(i, j):
+    # Returns an (i, j) element, if present, and 'None', otherwise.
+    a=min([i,j])
+    b=max([i,j])
+    if (a,b) in gram_mat_func:
+        return gram_mat_func[(a,b)]
+    else:
+        return None
+
+head_p=re.compile(r'(\d+):(\S+)?')
+elmnt_p=re.compile(r'(\d+):(\S+)')
+
+for line in open(file_name, 'r'):
+    tokens = insert_ast(line.rstrip()).split()
+    m = re.match(head_p, tokens[0])
+    if m:
+        g = m.groups()
+        if g[1] != None:
+            i = int(g[0])
+            d = max([d,i+1])
+            labels[i] = g[1]
+        else:
+            i = int(g[0])
+            for t in tokens[1:]:
+                g = re.match(elmnt_p, t).groups()
+                j = int(g[0])
+                set_elmnt(i,j,sympy.sympify(g[1]))
+
+# The dictionary 'labels' is converted into a list object
+labels=[labels[i] for i in range(0,d)]
+
+
+
+
+'''
+************************ End of kernel file processing *************************
+'''
+
 from sklearn.model_selection import cross_val_score
 from sklearn import svm
 import numpy as np
--- overall.py
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/python
-
-# take in input parameters: i.e. path to the .kernel file
-
-# record the labels and the sympy gram matrix
